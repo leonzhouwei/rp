5c140e0a2ff68aecd06df1f990493e091c93a3a4
Wei Zhou <leonzhouwei@gmail.com>
2016-05-31 00:26:46 +0800

    it works

---
+++ common/common.go
@@ -0,0 +1,106 @@
+package common
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+
+	"github.com/qiniu/log.v1"
+)
+
+const (
+	sql    = "select value from cpu"
+	points = "cpu,host=s1 value=10 1"
+)
+
+type QueryRet struct {
+	Results []Result `json:"results,omitempty"`
+	Err     error      `json:"error,omitempty"`
+}
+
+func (e QueryRet) String() string {
+	bytes, err := json.Marshal(e)
+	if err != nil {
+		log.Error(err)
+		return ""
+	}
+
+	return string(bytes)
+}
+
+type Result struct {
+	Series []Serie `json:"series,omitempty"`
+	Err    string    `json:"error,omitempty"`
+}
+
+func (e Result) String() string {
+	bytes, err := json.Marshal(e)
+	if err != nil {
+		log.Error(err)
+		return ""
+	}
+
+	return string(bytes)
+}
+
+type Serie struct {
+	Name    string            `json:"name"`
+	Columns []string          `json:"columns"`
+	Tags    map[string]string `json:"tags"`
+	Values  [][]interface{}   `json:"values"`
+}
+
+func (e Serie) String() string {
+	bytes, err := json.Marshal(e)
+	if err != nil {
+		log.Error(err)
+		return ""
+	}
+
+	return string(bytes)
+}
+
+func WritePoints(
+	host string, port int, db string, points string) error {
+
+	addr := host + ":" + strconv.Itoa(port)
+	url := "http://" + addr + "/write?db=" + db
+	log.Info("Write points query:", points)
+	body := strings.NewReader(points)
+	_, err := http.Post(url, "text/plain", body)
+
+	return err
+}
+
+func QueryInfluxdb(host string, port int, db string, sql string) (ret QueryRet, err error) {
+	
+	addr := host + ":" + strconv.Itoa(port)
+	queryURL := "http://" + addr + "/query?db=" + db + "&q=" + url.QueryEscape(sql)
+	resp, err := http.Get(queryURL)
+	if resp != nil {
+		defer resp.Body.Close()
+	}
+	if err != nil {
+		return
+	}
+	if resp.StatusCode != 200 {
+		err = fmt.Errorf("Request [%v] Error: %v", queryURL, resp)
+		return
+	}
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return
+	}
+
+	err = json.Unmarshal(data, &ret)
+	if err != nil {
+		return
+	}
+
+	return
+}
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/qiniu/log.v1"
+	"tmp/common"
+)
+
+const (
+	sql    = "select value from cpu"
+	points = "cpu,host=s1 value=10 1"
+)
+
+func main() {
+	host := "127.0.0.1"
+	port := 8086
+	db := "mydb"
+
+	// write
+//	err := common.WritePoints(host, port, db, points)
+//	if err != nil {
+//		log.Fatal(err)
+//	}
+
+	// read
+	result, err := common.QueryInfluxdb(host, port, db, sql)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Info(result)
+}
